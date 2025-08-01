572fc9ca8c07d1b1d2ae51ce7af83cf791e4496c
David B <dbenshachar@gmail.com>
2025-07-31 20:47:35 -0700

    Improve depth first search and add breadth first search.

--- binary_trees/breadth_first_search.py
+++ binary_trees/breadth_first_search.py
@@ -1,19 +1,33 @@
 from tree import node
+from queue import Queue
 
-def print_depth_first(head : node):
+def print_breadth_first(head : node):
     if head is None:
-        return None
+        return
     
     if head.value is not None:
         print(head.value, end=" ")
+    q = Queue()
+    q.put(head)
+    _print_breadth_first(q)
 
-    if head.left is not None:
-        print_depth_first(head.left)
+def _print_breadth_first(q : Queue):
+    head : node = q.get()
+
+    if head is None:
+        return None
+
+    if head.left is not None and head.left.value is not None:
+        print(head.left.value, end=" ")
+    q.put(head.left)
+
+    if head.right is not None and head.right.value is not None:
+        print(head.right.value, end=" ")
+    q.put(head.right)
+
+    _print_breadth_first(q)
     
-    if head.right is not None:
-        print_depth_first(head.right)
-    
-    return None
+
 
 #      1
 #    2   3
@@ -23,32 +37,32 @@ def _run_test_1():
     print("Test 1 output: ", end="")
     tree = node(1,
                 left=node(2,
-                        left=node(3),
-                        right=node(4)),
-                right=node(5,
+                        left=node(4),
+                        right=node(5)),
+                right=node(3,
                         left=None,
                         right=node(6)),
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_2():
     print("Test 2 output: ", end="")
     tree = node(value=None, left=None, right=None)
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_3():
     print("Test 3 output: ", end="")
     tree = node(1,
                 left=node(2,
-                        left=node(2, left=node(3), right=node(4)),
+                        left=node(4, left=node(6), right=node(7)),
                         right=node(5)),
-                right=node(6,
+                right=node(3,
                         left=None,
                         right=None),
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_4():
@@ -59,7 +73,7 @@ def _run_test_4():
                         right=None,
                 )
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_5():
@@ -70,19 +84,19 @@ def _run_test_5():
                         right=None,
                 )
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_6():
     print("Test 6 output: ", end="")
     tree = node(value=None, left=node(1), right=None)
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_7():
     print("Test 7 output: ", end="")
     tree = node(value=None, left=None, right=node(1))
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_8():
@@ -93,25 +107,25 @@ def _run_test_8():
                         right=None,
                 )
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_9():
     print("Test 9 output: ", end="")
     tree = node(None,
-                right=node(2,
+                left=node(None, right=node(2)),
+                right=node(1,
                         left=node(None, left=node(None, left=node(3))),
                         right=None,
                 ),
-                left=node(None, right=node(1))
             )
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 def _run_test_10():
     print("Test 10 output: ", end="")
     tree = None
-    print_depth_first(tree)
+    print_breadth_first(tree)
     print("")
 
 if __name__ == "__main__":
