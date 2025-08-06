27e97585ea7c3b792862b25cf7f61c065f878606
David B <dbenshachar@gmail.com>
2025-08-06 10:37:09 -0700

    .

--- binary_trees/depth_first_search.py
+++ binary_trees/depth_first_search.py
@@ -1,6 +1,7 @@
 from tree import node
+from queue import Queue
 
-def print_depth_first(head : node):
+def print_depth_first_recursion(head : node):
     if head is None:
         return None
     
@@ -8,10 +9,10 @@ def print_depth_first(head : node):
         print(head.value, end=" ")
 
     if head.left is not None:
-        print_depth_first(head.left)
+        print_depth_first_recursion(head.left)
     
     if head.right is not None:
-        print_depth_first(head.right)
+        print_depth_first_recursion(head.right)
     
     return None
 
@@ -19,7 +20,7 @@ def print_depth_first(head : node):
 #    2   3
 #   4 5 ? 6
 
-def _run_test_1():
+def _run_test_1(print_depth_first):
     print("Test 1 output: ", end="")
     tree = node(1,
                 left=node(2,
@@ -32,13 +33,13 @@ def _run_test_1():
     print_depth_first(tree)
     print("")
 
-def _run_test_2():
+def _run_test_2(print_depth_first):
     print("Test 2 output: ", end="")
     tree = node(value=None, left=None, right=None)
     print_depth_first(tree)
     print("")
 
-def _run_test_3():
+def _run_test_3(print_depth_first):
     print("Test 3 output: ", end="")
     tree = node(1,
                 left=node(2,
@@ -51,7 +52,7 @@ def _run_test_3():
     print_depth_first(tree)
     print("")
 
-def _run_test_4():
+def _run_test_4(print_depth_first):
     print("Test 4 output: ", end="")
     tree = node(1,
                 left=node(2,
@@ -62,7 +63,7 @@ def _run_test_4():
     print_depth_first(tree)
     print("")
 
-def _run_test_5():
+def _run_test_5(print_depth_first):
     print("Test 5 output: ", end="")
     tree = node(1,
                 right=node(2,
@@ -73,19 +74,19 @@ def _run_test_5():
     print_depth_first(tree)
     print("")
 
-def _run_test_6():
+def _run_test_6(print_depth_first):
     print("Test 6 output: ", end="")
     tree = node(value=None, left=node(1), right=None)
     print_depth_first(tree)
     print("")
 
-def _run_test_7():
+def _run_test_7(print_depth_first):
     print("Test 7 output: ", end="")
     tree = node(value=None, left=None, right=node(1))
     print_depth_first(tree)
     print("")
 
-def _run_test_8():
+def _run_test_8(print_depth_first):
     print("Test 8 output: ", end="")
     tree = node(1,
                 right=node(2,
@@ -96,7 +97,7 @@ def _run_test_8():
     print_depth_first(tree)
     print("")
 
-def _run_test_9():
+def _run_test_9(print_depth_first):
     print("Test 9 output: ", end="")
     tree = node(None,
                 right=node(2,
@@ -108,20 +109,21 @@ def _run_test_9():
     print_depth_first(tree)
     print("")
 
-def _run_test_10():
+def _run_test_10(print_depth_first):
     print("Test 10 output: ", end="")
     tree = None
     print_depth_first(tree)
     print("")
 
 if __name__ == "__main__":
-    _run_test_1()
-    _run_test_2()
-    _run_test_3()
-    _run_test_4()
-    _run_test_5()
-    _run_test_6()
-    _run_test_7()
-    _run_test_8()
-    _run_test_9()
-    _run_test_10()
+    fn = print_depth_first_recursion
+    _run_test_1(fn)
+    _run_test_2(fn)
+    _run_test_3(fn)
+    _run_test_4(fn)
+    _run_test_5(fn)
+    _run_test_6(fn)
+    _run_test_7(fn)
+    _run_test_8(fn)
+    _run_test_9(fn)
+    _run_test_10(fn)
